b65bb896e2e32e1ed9fd136a990725ccca77449f
Jamie Murphy <jamie@itjamie.com>
2013-11-27 08:25:50 +0000

    v0.1

---
+++ example.php
@@ -0,0 +1,24 @@
+<?php
+
+// Here are the minimum requirements to login and get back the first page. Supports http and https and offset ports.
+$inputarray['username']	= 'ubnt';
+$inputarray['password']	= 'ubnt';
+$inputarray['cookiefile'] = '/home/user/airos/cookies/192.168.1.2.txt';
+
+$inputarray['requestpage'] =  '/brmacs.cgi?brmacs=y'; 
+$inputarray['expectedpage'] =  'https://192.168.1.2:8443/brmacs.cgi?brmacs=y';  
+$inputarray['loginurl'] =  'http://192.168.1.2:8443/login.cgi'; 	 
+
+
+// Here you can request extra pages once the first page has been logged into. This is not required.
+$extrapages[0]['page-name'] = 'status';
+$extrapages[0]['page-url'] = 'http://192.168.1.2:8443/status.cgi?';
+
+$extrapages[1]['page-name'] = 'stations';
+$extrapages[1]['page-url'] = 'http://192.168.1.2:8443/sta.cgi?';
+
+
+$reply = get_pages_from_ubnt_airos_device($inputarray, $extrapages);
+
+
+print_r($reply);
