8d2d4584118467bcae930621dfe6fac69b200883
Jamie Murphy <jamiemurphyit@gmail.com>
2013-11-27 12:28:32 +0000

    Update example.php
    Added missing http(s)

--- example.php
+++ example.php
@@ -9,15 +9,15 @@ $inputarray['cookiefile'] = '/home/user/airos/cookies/192.168.1.2.txt';
 
 $inputarray['requestpage'] =  '/brmacs.cgi?brmacs=y'; 
 $inputarray['expectedpage'] =  'https://192.168.1.2:8443/brmacs.cgi?brmacs=y';  
-$inputarray['loginurl'] =  'http://192.168.1.2:8443/login.cgi'; 	 
+$inputarray['loginurl'] =  'https://192.168.1.2:8443/login.cgi'; 	 
 
 
 // Here you can request extra pages once the first page has been logged into. This is not required.
 $extrapages[0]['page-name'] = 'status';
-$extrapages[0]['page-url'] = 'http://192.168.1.2:8443/status.cgi?';
+$extrapages[0]['page-url'] = 'https://192.168.1.2:8443/status.cgi?';
 
 $extrapages[1]['page-name'] = 'stations';
-$extrapages[1]['page-url'] = 'http://192.168.1.2:8443/sta.cgi?';
+$extrapages[1]['page-url'] = 'https://192.168.1.2:8443/sta.cgi?';
 
 
 $reply = get_pages_from_ubnt_airos_device($inputarray, $extrapages);
